07d6de8426d008db0c34ca2b008f9b188866387f
Madusha Lakshan <84628815+madusha026@users.noreply.github.com>
2025-05-29 13:26:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import {DM_Sans } from "next/font/google";
 import Navbar from "./components/navbar/navbar";
-import Footer from "./components/footer/footer";
+
 import "./globals.css";
 
 
